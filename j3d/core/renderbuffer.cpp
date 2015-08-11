9bd066027814a5875ecd6ace84f89638398c2dbf
Will Justis <will@jawdev.com>
2015-08-11 16:59:32 -0600

    using default renderbuffer for now... that means only billboards and no sprites

--- j3d/core/renderbuffer.cpp
+++ j3d/core/renderbuffer.cpp
@@ -15,26 +15,27 @@ Renderbuffer::Renderbuffer()
 {
 	m_width = engine::config()->window_width;
 	m_height = engine::config()->window_height;
-	mp_renderbuffers = new GLuint[NUM_BUFFERS];
+	//mp_renderbuffers = new GLuint[NUM_BUFFERS];
 	init();
 }
 
 Renderbuffer::~Renderbuffer()
 {
-	J3D_SAFE_DELETE_ARR(mp_renderbuffers);
+	//J3D_SAFE_DELETE_ARR(mp_renderbuffers);
 }
 
 void Renderbuffer::reshape(int w, int h)
 {
 	m_width = engine::config()->window_width;
 	m_height = engine::config()->window_height;
-	glDeleteRenderbuffers(NUM_BUFFERS, mp_renderbuffers);
-	glDeleteFramebuffers(1, &m_framebuffer);
+	//glDeleteRenderbuffers(NUM_BUFFERS, mp_renderbuffers);
+	//glDeleteFramebuffers(1, &m_framebuffer);
 	init();
 }
 
 void Renderbuffer::init()
 {
+/*
 	glGenRenderbuffers(NUM_BUFFERS, mp_renderbuffers);
 	glBindRenderbuffer(GL_RENDERBUFFER, mp_renderbuffers[COLOR_BUFFER]);
 	glRenderbufferStorage(GL_RENDERBUFFER, GL_RGBA, m_width, m_height);
@@ -48,23 +49,26 @@ void Renderbuffer::init()
 			GL_RENDERBUFFER, mp_renderbuffers[COLOR_BUFFER]);
 	glFramebufferRenderbuffer(GL_DRAW_FRAMEBUFFER, GL_DEPTH_ATTACHMENT,
 			GL_RENDERBUFFER, mp_renderbuffers[DEPTH_BUFFER]);
+*/
 }
 
 void Renderbuffer::bind()
 {
-	glClearColor(0.5f, 0, 0, 0.1f);
 	glEnable(GL_CULL_FACE);
 	glEnable(GL_DEPTH_TEST);
-	glBindFramebuffer(GL_DRAW_FRAMEBUFFER, m_framebuffer);
+	//glBindFramebuffer(GL_DRAW_FRAMEBUFFER, m_framebuffer);
+	glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
 	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
 }
 
 void Renderbuffer::blit()
 {
+/*
 	glBindFramebuffer(GL_READ_FRAMEBUFFER, m_framebuffer);
 	glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
 	glBlitFramebuffer(0, 0, m_width - 1, m_height - 1, 0, 0, m_width - 1,
 			m_height - 1, GL_COLOR_BUFFER_BIT, GL_LINEAR);
+*/
 }
 
 } }
